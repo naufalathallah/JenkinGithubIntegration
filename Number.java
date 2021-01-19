994c6a21b0f25dad281cd9f02f8aeef1e7b0e332
Naufal Athallah Iwel <naufalaiwel@gmail.com>
2021-01-19 21:58:48 +0700

    bilangan prima

--- Number.java
+++ Number.java
@@ -1,7 +1,27 @@
 public class Number {
 public static void main(String args[]) {
-for (int i=0; i<10; i++){
-System.out.println(i);
-}
+int i =0;
+       int num =0;
+       //Empty String
+       String  primeNumbers = "";
+
+       for (i = 1; i <= 100; i++)         
+       { 		  	  
+          int counter=0; 	  
+          for(num =i; num>=1; num--)
+	  {
+             if(i%num==0)
+	     {
+ 		counter = counter + 1;
+	     }
+	  }
+	  if (counter ==2)
+	  {
+	     //Appended the Prime number to the String
+	     primeNumbers = primeNumbers + i + " ";
+	  }	
+       }	
+       System.out.println("Prime numbers from 1 to 100 are :");
+       System.out.println(primeNumbers);
 }
 }
